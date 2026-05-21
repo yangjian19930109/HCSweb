96f1024a794de045be803fb8232b6693227c0eec
DaiKexing <daikexing@example.com>
2026-05-21 17:04:21 +0800

    fix: 修正static-server根目录路径

--- static-server.js
+++ static-server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const root = path.resolve('C:/Users/11193/www');
+const root = path.resolve('C:/Users/11193/Desktop/www');
 const PORT = 8080;
 
 const MIME = {
